f97eab9e8f523c8969881619abd9477c25a4f9f5
Huangxianan <xianan_huang@163.com>
2015-10-10 00:44:37 +0800

    yii2.0插件-百度编辑器ueditor
    在yii2.0.6中整合了百度编辑器ueditor，安装文件，使用正确的调用方式即可使用

---
+++ assets/UeditorAsset.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link http://www.yii-china.com/
+ * @copyright Copyright (c) 2015 Yii中文网
+ */
+
+namespace common\widgets\ueditor\assets;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * @author Xianan Huang <xianan_huang@163.com>
+ */
+class UeditorAsset extends AssetBundle
+{
+    public $css = [
+
+    ];
+    
+    public $js = [
+        'ueditor.config.js',
+        'ueditor.all.js',
+    ];
+    
+    public $depends = [
+
+    ];
+    
+    /**
+     * 初始化：sourcePath赋值
+     * @see \yii\web\AssetBundle::init()
+     */
+    public function init()
+    {
+        $this->sourcePath = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR . 'vendor';
+    }
+}
