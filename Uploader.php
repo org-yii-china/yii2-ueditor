d338c630d53e2fb1e11879abc779df7ce6c14646
Huangxianan <xianan_huang@163.com>
2016-03-30 13:12:34 +0800

    Update Uploader.php

--- Uploader.php
+++ Uploader.php
@@ -268,7 +268,7 @@ class Uploader
     private function getFilePath()
     {
         $fullname = $this->fullName;
-        $rootPath = $_SERVER['DOCUMENT_ROOT'];
+        $rootPath = isset($this->config['uploadFilePath'])&&!empty($this->config['uploadFilePath'])?$this->config['uploadFilePath']:$_SERVER['DOCUMENT_ROOT'];
         if (substr($fullname, 0, 1) != '/') {
             $fullname = '/' . $fullname;
         }
